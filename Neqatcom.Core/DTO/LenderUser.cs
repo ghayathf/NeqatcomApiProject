c14a4744819d71cd6e4818ffa19438fdb56c3616
BushraKhattabAlawneh <boshraalawneh6201@outlook.com>
2023-04-18 01:36:54 +0300

    bb

--- Neqatcom.Core/DTO/LenderUser.cs
+++ Neqatcom.Core/DTO/LenderUser.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Neqatcom.Core.DTO
 {
-    internal class LenderUser
+    public class LenderUser
     {
+        public decimal Userid { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phonenum { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public string Username { get; set; }
+        public string Userimage { get; set; }
+
+        public decimal Lenderid { get; set; }
+        public string Commercialregister { get; set; }
+        //public decimal? Userid { get; set; }
+        public decimal? Registerstatus { get; set; }
+        public decimal? Shadowstatus { get; set; }
+        public string Companysize { get; set; }
+        public string Siteurl { get; set; }
     }
 }
--- Neqatcom.Core/DTO/LoaneeUser.cs
+++ Neqatcom.Core/DTO/LoaneeUser.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Neqatcom.Core.DTO
 {
-    internal class LoaneeUser
+    public class LoaneeUser
     {
+        public decimal Userid { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phonenum { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public string Username { get; set; }
+        public string Userimage { get; set; }
+
+        public decimal Loaneeid { get; set; }
+        public string Nationalnumber { get; set; }
+        public DateTime? Dateofbirth { get; set; }
+        public decimal? Salary { get; set; }
+        public decimal? Numoffamily { get; set; }
+        public decimal? Creditscore { get; set; }
+        //public decimal? Userid { get; set; }
     }
 }
