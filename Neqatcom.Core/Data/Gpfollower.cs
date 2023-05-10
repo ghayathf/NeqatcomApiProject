bcf090713e3e841ec67f0ae85ed8d16980d0a523
GhayathQuraan <ghayath04@hotmail.com>
2023-05-11 02:35:37 +0300

    ASDF

---
+++ Neqatcom.Core/Data/Gpfollower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Neqatcom.Core.Data
+{
+    public partial class Gpfollower
+    {
+        public decimal Followersid { get; set; }
+        public decimal? Lenderid { get; set; }
+        public decimal? Loaneeid { get; set; }
+
+        public virtual Gplenderstore Lender { get; set; }
+        public virtual Gploanee Loanee { get; set; }
+    }
+}
