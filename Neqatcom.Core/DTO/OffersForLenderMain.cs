f69af8b37cf06246bf5c36bc0e46f3ae83b31272
GhayathQuraan <ghayath04@hotmail.com>
2023-05-17 23:08:15 +0300

    bb

---
+++ Neqatcom.Core/DTO/OffersForLenderMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neqatcom.Core.DTO
+{
+    public class OffersForLenderMain
+    {
+        public decimal Categoryid { get; set; }
+        public string Categoryname { get; set; }
+        public string Categoryimage { get; set; }
+        public decimal Offerid { get; set; }
+        public decimal? Totalmonths { get; set; }
+        public string Descriptions { get; set; }
+        public decimal? Lenderid { get; set; }
+        public decimal? Minmonth { get; set; }
+    }
+}
